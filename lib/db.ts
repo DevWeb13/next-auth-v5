9b1b9573d73513cde5d0587f023f2e3e776c8f4b
DevWeb13 <giuliano.loic@sfr.fr>
2024-01-13 11:41:00 +0100

    Database & Prisma setup

---
+++ lib/db.ts
@@ -0,0 +1,13 @@
+// lib/db.ts
+
+import { PrismaClient } from '@prisma/client';
+
+export const db = globalThis.prisma || new PrismaClient();
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === 'development') {
+  globalThis.prisma = db;
+}
