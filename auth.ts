67808dbebc4a294ba5c2ed43a4df4843e6a36028
DevWeb13 <giuliano.loic@sfr.fr>
2024-01-14 11:36:12 +0100

    Callbacks

--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 
 import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
+import { getUserByID } from '@/data/user';
+import { UserRole } from '@prisma/client';
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +12,31 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  // https://authjs.dev/guides/basics/callbacks
+  callbacks: {
+    async session({ token, session }) {
+      console.log('session', token);
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserByID(token.sub);
+
+      token.role = existingUser?.role;
+
+      if (!existingUser) return token;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
